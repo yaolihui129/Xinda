244a1abb7e87c1c79e8398e25edfa1a6e84144d2
yaolihui129 <yaolihui129@sina.com>
2017-05-15 18:13:01 +0800

    员工管理新增和编辑功能

--- Apps/Admin/Controller/StaffController.class.php
+++ Apps/Admin/Controller/StaffController.class.php
@@ -24,46 +24,100 @@ class StaffController extends CommonController {
     
     
     public function insert(){
-        $m=D('tp_staff');
-        $_POST['moder']=$_SESSION['realname'];
-        $_POST['prodid']=$_SESSION['prodid'];
-        //处理上传图片
-        $upload = new \Think\Upload();// 实例化上传类
-        $upload->maxSize  =     7145728 ;// 设置附件上传大小
-        $upload->exts     =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-        $upload->rootPath =  './Upload/'.getQz($_SESSION['prodid']).'/';// 设置附件上传目录
-        $upload->savePath = '/Staff/'; // 设置附件上传目录
-        $info  =   $upload->upload();
-    
-        if(!$info) {// 上传错误提示错误信息或没有上传图片
-            if(!$m->create()){
-                $this->error($m->getError());
+        $where=array('phone'=>$_POST['username']);
+        $m=D('tp_credit');
+        $arr=$m->where($where)->find();
+        if($arr){//检查征信表
+            $m=D('tp_staff');
+            $_POST['moder']=$_SESSION['realname'];
+            $_POST['prodid']=$_SESSION['prodid'];
+            $upload = new \Think\Upload(); //处理上传图片
+            $upload->maxSize  =     7145728 ;// 设置附件上传大小
+            $upload->exts     =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+            $upload->rootPath =     './Upload/'.getQz($_SESSION['prodid']).'/';// 设置附件上传目录
+            $upload->savePath =     '/Staff/'; // 设置附件上传目录
+            $info  =   $upload->upload();           
+            if(!$info) {// 上传错误提示错误信息或没有上传图片
+                if(!$m->create()){
+                    $this->error($m->getError());
+                }
+                if($m->add()){
+                    $this->success("成功",U('index'));
+                }else{
+                    $this->error('失败');
+                }
+            }else{// 上传成功 获取上传文件信息
+                $_POST['img']=getQz($_SESSION['prodid']).$info['img']['savepath'].$info['img']['savename'];
+                if(!$m->create()){
+                    $this->error($m->getError());
+                }
+                if($m->add()){//等比例缩放
+                    $image = new \Think\Image();
+                    $image->open('./Upload/'.getQz($_SESSION['prodid']).'/'.$info['img']['savepath'].$info['img']['savename']);
+                    $image->thumb(800, 400)->save('./Upload/'.getQz($_SESSION['prodid']).'/'.$info['img']['savepath'].$info['img']['savename']);
+                    $this->success("成功",U('index'));
+                }else{
+                    $this->error('失败');
+                }
             }
-            if($m->add()){
-                $this->success("成功",U('index'));
-            }else{
-                $this->error('失败');
+        }else {//向征信表插入数据
+            do {//如果该ID在库中存在，则重新获取
+                $id=getRandCode(9);
+                $arr=$m->find($id);
+            } while ($arr);
+            $_POST['creditid']=$id;
+            $_POST['phone']=$_POST['username'];
+            $_POST['realname']=$_POST['realname'];
+            $_POST['password']=md5('123456');
+            $_POST['adder']=$_SESSION['realname'];
+            $_POST['moder']=$_SESSION['realname'];
+            $_POST['ctime']=time();
+            $m->create();
+            $m->add();
+            $arr=$m->where($where)->find();
+            //向员工表插入数据
+            $m=D('tp_staff');
+            $_POST['moder']=$_SESSION['realname'];
+            $_POST['prodid']=$_SESSION['prodid'];
+            $upload = new \Think\Upload(); //处理上传图片
+            $upload->maxSize  =     7145728 ;// 设置附件上传大小
+            $upload->exts     =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+            $upload->rootPath =     './Upload/'.getQz($_SESSION['prodid']).'/';// 设置附件上传目录
+            $upload->savePath =     '/Staff/'; // 设置附件上传目录
+            $info  =   $upload->upload();
+            if(!$info) {// 上传错误提示错误信息或没有上传图片
+                if(!$m->create()){
+                    $this->error($m->getError());
+                }
+                if($m->add()){
+                    $this->success("成功",U('index'));
+                }else{
+                    $this->error('失败');
+                }
+            }else{// 上传成功 获取上传文件信息
+                $_POST['img']=getQz($_SESSION['prodid']).$info['img']['savepath'].$info['img']['savename'];
+                if(!$m->create()){
+                    $this->error($m->getError());
+                }
+                if($m->add()){//等比例缩放
+                    $image = new \Think\Image();
+                    $image->open('./Upload/'.getQz($_SESSION['prodid']).'/'.$info['img']['savepath'].$info['img']['savename']);
+                    $image->thumb(800, 400)->save('./Upload/'.getQz($_SESSION['prodid']).'/'.$info['img']['savepath'].$info['img']['savename']);
+                    $this->success("成功",U('index'));
+                }else{
+                    $this->error('失败');
+                }
             }
-        }else{// 上传成功 获取上传文件信息
-            $_POST['img']=getQz($_SESSION['prodid']).$info['img']['savepath'].$info['img']['savename'];
-            if(!$m->create()){
-                $this->error($m->getError());
-            }
-            if($m->add()){//等比例缩放
-                $image = new \Think\Image();
-                $image->open('./Upload/'.getQz($_SESSION['prodid']).'/'.$info['img']['savepath'].$info['img']['savename']);
-                $image->thumb(800, 400)->save('./Upload/'.getQz($_SESSION['prodid']).'/'.$info['img']['savepath'].$info['img']['savename']);
-                $this->success("成功",U('index'));
-            }else{
-                $this->error('失败');
-            }
+            
         }
+        
+        
+        
+        
     }
     
     public function mod(){
-        /* 实例化模型*/
-        $m=D('tp_staff');
-        $arr=$m->find($_GET[id]);
+        $arr=M('tp_staff')->find($_GET[id]);
         $this->assign('arr',$arr);
     
         $this->display();
@@ -71,13 +125,12 @@ class StaffController extends CommonController {
     
     
     public function update(){
-        $_POST['moder']=$_SESSION['realname'];
-        //处理上传图片
-        $upload = new \Think\Upload();// 实例化上传类
+        $_POST['moder']=$_SESSION['realname'];      
+        $upload = new \Think\Upload();//处理上传图片
         $upload->maxSize  =     7145728 ;// 设置附件上传大小
         $upload->exts     =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-        $upload->rootPath =  './Upload/'.getQz($_SESSION['prodid']).'/';// 设置附件上传目录
-        $upload->savePath = '/Staff/'; // 设置附件上传目录
+        $upload->rootPath =     './Upload/'.getQz($_SESSION['prodid']).'/';// 设置附件上传目录
+        $upload->savePath =     '/Staff/'; // 设置附件上传目录
         $info  =   $upload->upload();//上传图片
         if(!$info) {// 上传错误提示错误信息
             if (D('tp_staff')->save($_POST)){
