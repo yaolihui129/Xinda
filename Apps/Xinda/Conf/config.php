5fabcafc12617071911845c9f3b80e557a0a37c1
yaolihui129 <yaolihui129@sina.com>
2017-05-18 19:54:41 +0800

    完成焦点轮播图12

--- Apps/Xinda/Conf/config.php
+++ Apps/Xinda/Conf/config.php
@@ -2,6 +2,7 @@
 return array(
 	//'配置项'=>'配置值'
     'TMPL_PARSE_STRING'=>array(
+        '__CSS__'=>__ROOT__.'/Public/css',
         '__IMAGES__'=>__ROOT__.'/Public/images/Xinda',
         '__IMG__'=>__ROOT__.'/Public/images',
         '__UPLOAD__'=>__ROOT__.'/Upload'
