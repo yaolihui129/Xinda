a9dbf2891128ace6fea444f1ff72cebf7ff0493a
yaolihui129 <yaolihui129@sohu.com>
2018-02-23 10:25:00 +0800

    添加远程数据库连接

--- Apps/Record/Conf/config.php
+++ Apps/Record/Conf/config.php
@@ -12,10 +12,18 @@ return array(
     'PRODUCT'       =>  'Record',//定义产品编号
     'WEIXIN_APPID'  =>  '',//定义微信APPID
     //连接数据库
-    'DB_HOST'       =>  '192.168.155.54',   
-    'DB_NAME'       =>  'zentao',
-    'DB_USER'       =>  'root',
-    'DB_PWD'        =>  'chexian',
-    'DB_PORT'       =>  '3306',
+    //公司本地数据库
+//    'DB_HOST'       =>  '192.168.155.54',
+//    'DB_NAME'       =>  'zentao',
+//    'DB_USER'       =>  'root',
+//    'DB_PWD'        =>  'chexian',
+//    'DB_PORT'       =>  '3306',
+
+    //线上数据库
+    'DB_HOST'=>'2lbrknae.2248.dnstoo.com',
+    'DB_NAME'=>'xiuli',
+    'DB_USER'=>'xiuli_f',
+    'DB_PWD'=>'yaolihui0506',
+    'DB_PORT'=>'5503',
 
 );
--- Apps/Tpyjd/Controller/ActivityController.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Tpyjd\Controller;
-class ActivityController extends WebInfoController {
-    public function index(){
-        $this->display();
-    }
-}
